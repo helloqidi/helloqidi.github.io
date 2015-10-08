69461933f30aba7357a2f9c7b20f60a789ab630a
helloqidi <helloqidi@gmail.com>
2015-10-08 16:23:17 +0800

    init

--- files/看板/data.js
+++
@@ -1,7 +0,0 @@
-﻿$axure.loadCurrentPage(
-(function() {
-    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,c,d,e,f,g,h,[i],j,_(k,l,m,n,o,p,q,_(r,s),t,_(u,v,w,x,y,_(z,A,B,C),D,null,E,x,F,x,G,H,I,null,J,K,L,M,N,O,P,K),Q,_(),R,_(),S,_(T,[])),U,_(),V,_());}; 
-var b="url",c="看板.html",d="generationDate",e=new Date(1427963471457.59),f="isCanvasEnabled",g=false,h="variables",i="OnLoadVariable",j="page",k="packageId",l="dbfb02c2d7724ef7a1dce8b8a799f860",m="type",n="Axure:Page",o="name",p="看板",q="notes",r="Note",s="<p><span>用户登录后看到的</span><span>第一个页面。可用于发现异常，也是一些操作的快捷入口</span></p>",t="style",u="baseStyle",v="627587b6038d43cca051c114ac41ad32",w="pageAlignment",x="near",y="fill",z="fillType",A="solid",B="color",C=0xFFFFFFFF,D="image",E="imageHorizontalAlignment",F="imageVerticalAlignment",G="imageRepeat",H="auto",I="favicon",J="sketchFactor",K="0",L="colorStyle",M="appliedColor",N="fontName",O="Applied Font",P="borderWidth",Q="adaptiveStyles",R="interactionMap",S="diagram",T="objects",U="masters",V="objectPaths";
-return _creator();
-})());
